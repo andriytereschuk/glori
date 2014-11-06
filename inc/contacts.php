352ef4839a13a39d5130be159c30c43cbd73ec3f
andrii.tereshchuk <andrii.tereshchuk@eleks.com>
2014-11-06 17:19:52 +0200

    changed img foe slide Services added phone numbers and linkedin link fixing bug

--- inc/contacts.php
+++ inc/contacts.php
@@ -3,9 +3,18 @@
 		<i class="icon-logo3"></i>
 		<i class="icon-blue-arrow"></i>
 
-		<div class="con"><span>Телефон:</span>+ 3 8097 77 15 333</div>
-		<div class="con"><span>E-mail:</span>glory@gmail.com</div>
+		<div class="con2">
+			<span>Телефони:</span>
+			<div>
+				+380667917872<br>
+				+380962580135<br>
+				+380937278682<br>
+			</div>
+
+		</div>
+		<div class="clr"></div>
+		<div class="con"><span>E-mail:</span>info@glorynco.com</div>
 		<a href="https://www.facebook.com/gloryandcompany" target="_blank" class="icon-facebook"></a>
-		<a href="#" class="icon-in"></a>
+		<a href="https://www.linkedin.com/company/glory-&-co-" class="icon-in"></a>
 	</div>
 </div>
